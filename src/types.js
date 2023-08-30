b5d9ffb5559473cc948cf54427069d19f24556fa
Svetlana Sedova <svt.sedova@gmail.com>
2023-08-30 21:46:01 +0300

    Добавляет отрисовку фильтров и сортировки динамически на основании полученных данных

--- src/types.js
+++ src/types.js
@@ -1,57 +1,76 @@
 /**
  * @typedef {{
- *  id: string
- *  base_price: number
- *  date_from: string
- *  date_to: string
- *  destination: string
- *  is_favorite: boolean
- *  offers: Array<string>
- *  type: PointType
- * }} Point
- */
+*  id: string
+*  base_price: number
+*  date_from: string
+*  date_to: string
+*  destination: string
+*  is_favorite: boolean
+*  offers: Array<string>
+*  type: PointType
+* }} Point
+*/
 
 /**
- * @typedef {{
- *  id: string
- *  name: string
- *  description: string
- *  pictures: Array<Picture>
- * }} Destination
- */
+* @typedef {{
+*  id: string
+*  name: string
+*  description: string
+*  pictures: Array<Picture>
+* }} Destination
+*/
 
 /**
- * @typedef {{
- *  src: string
- *  description: string
- * }} Picture
- */
+* @typedef {{
+*  src: string
+*  description: string
+* }} Picture
+*/
 
 /**
- * @typedef {{
- *  type: PointType
- *  offers: Array<Offer>
- * }} OfferSet
- */
+* @typedef {{
+*  type: PointType
+*  offers: Array<Offer>
+* }} OfferSet
+*/
 
 /**
- * @typedef {{
- *  id: string
- *  title: string
- *  price: number
- * }} Offer
- */
+* @typedef {{
+*  id: string
+*  title: string
+*  price: number
+* }} Offer
+*/
 
 /**
- * @typedef {(
- *   'taxi'
- * | 'bus'
- * | 'train'
- * | 'ship'
- * | 'drive'
- * | 'flight'
- * | 'check-in'
- * | 'sightseeing'
- * | 'restaurant'
- * )} PointType
- */
+* @typedef {(
+*   'taxi'
+* | 'bus'
+* | 'train'
+* | 'ship'
+* | 'drive'
+* | 'flight'
+* | 'check-in'
+* | 'sightseeing'
+* | 'restaurant'
+* )} PointType
+*/
+
+/**
+* @typedef {(
+*   'everything'
+* | 'future'
+* | 'present'
+* | 'past'
+* )} FilterType
+*/
+
+/**
+* @typedef {(
+*   'day'
+* | 'event'
+* | 'time'
+* | 'price'
+* | 'offers'
+* )} SortType
+*/
